d4c3c0df304711fe4953233177f04cfe06a07ab8
Emil Motrøen <e.motroen@gmail.com>
2025-01-11 00:44:51 +0100

    Add Unity Labs 1 & 2

---
+++ Unity/Unity Labs/Assets/BallBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+        Debug.DrawRay(Vector3.zero, axis, Color.yellow, .5f);
+    }
+}
